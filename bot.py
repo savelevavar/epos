755fd30773f247b8b987b02108538c0553c1bd1b
savelevavar <savelevavar@mail.ru>
2022-05-31 19:59:54 +0500

    first commit

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 import datetime
-
+from telebot import types
 
 bot = telebot.TeleBot('5154655086:AAEJatzVb9H163lz6pabcN7k3N7P6TdnuOU')
 
@@ -8,6 +8,13 @@ bot = telebot.TeleBot('5154655086:AAEJatzVb9H163lz6pabcN7k3N7P6TdnuOU')
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, f'Добро пожаловать в бота "Эпос". Авторизуйтесь для дальнейшей работы')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    student_type = types.KeyboardButton("Я ученик")
+    markup.add(student_type)
+    bot.send_message(message.chat.id,
+                     text="Выберите роль пользователя на портале", reply_markup=markup)
+    
+
 
 
 
