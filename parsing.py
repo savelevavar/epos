abbbfb572e8fc76c09f2dd2e2065c222f1b30e26
savelevavar <savelevavar@mail.ru>
2022-09-07 22:30:49 +0500

    work-work

--- parsing.py
+++ parsing.py
@@ -1,6 +1,8 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 
@@ -10,17 +12,27 @@ def init_driver():
     return driver
 
 
-def lookup(driver, query):
+def lookup(driver):
     driver.get("https://cabinet.permkrai.ru/login")
+    login = 'varya.saveleva.05@mail.ru'
+    password = 'qweasdzxc2289'
     try:
-        button = driver.find_element_by_class_name('login__item')
+        button = driver.wait.until(EC.element_to_be_clickable(
+            (By.CLASS_NAME, "login__item")))
         button.click()
+        box_login = driver.wait.until(EC.presence_of_element_located(
+            (By.CLASS_NAME, "base-input login__input login__input--login")))
+        box_password = driver.wait.until(EC.presence_of_element_located(
+            (By.CLASS_NAME, "base-input login__input login__input--password")))
+
+        box_login.send_keys(login)
+        box_password.send_keys(password)
     except TimeoutException:
-        print("Box or Button not found in google.com")
+        print("Box or Button not found in epos")
 
 
 if __name__ == "__main__":
     driver = init_driver()
-    lookup(driver, "7 признаков почему вы енот")
+    lookup(driver)
     time.sleep(5)
-    driver.quit()
+    # driver.quit()
